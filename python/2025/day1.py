0c6ec4fbd3866f3ff66e6fff8c3e6506db38067b
Sydney <68250402+cecelot@users.noreply.github.com>
2025-12-01 09:58:44 -0500

    refactor(2025/python): cleanup day 1:wq

--- python/2025/day1.py
+++ python/2025/day1.py
@@ -1,25 +1,21 @@
-def part1(ops: list[tuple[str, int]]) -> int:
-    dial = 50
-    password = 0
+def single(ops: list[tuple[str, int]]) -> int:
+    dial, password = 50, 0
     for (dir, n) in ops:
-        if dir == 'L': dial -= n
-        elif dir == 'R': dial += n
+        dial += -n if dir == 'L' else n
         dial %= 100
         if dial == 0: password += 1
     return password
 
-def part2(ops: list[tuple[str, int]]) -> int:
-    dial = 50
-    password = 0
+def any(ops: list[tuple[str, int]]) -> int:
+    dial, password = 50, 0
     for (dir, n) in ops:
         for _ in range(0, abs(n)):
-            if dir == 'L': dial -= 1
-            elif dir == 'R': dial += 1 
+            dial += -1 if dir == 'L' else 1
             dial %= 100
             if dial == 0: password += 1
     return password
 
 with open("input/day1.txt") as f:
     ops = [(x[0], int(str(x[1:]).strip())) for x in f.readlines()]
-    print(f'Part 1: {part1(ops)}')
-    print(f'Part 2: {part2(ops)}')
+    print(f'Part 1: {single(ops)}')
+    print(f'Part 2: {any(ops)}')
