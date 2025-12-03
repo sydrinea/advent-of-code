e3773b8c55ac49c3c637250f497f737d894733a2
Sydney <68250402+cecelot@users.noreply.github.com>
2025-12-03 09:07:14 -0500

    chore(python): format

--- python/2025/day1.py
+++ python/2025/day1.py
@@ -1,21 +1,25 @@
 def single(ops: list[tuple[str, int]]) -> int:
     dial, password = 50, 0
-    for (dir, n) in ops:
-        dial += -n if dir == 'L' else n
+    for dir, n in ops:
+        dial += -n if dir == "L" else n
         dial %= 100
-        if dial == 0: password += 1
+        if dial == 0:
+            password += 1
     return password
+
 
 def any(ops: list[tuple[str, int]]) -> int:
     dial, password = 50, 0
-    for (dir, n) in ops:
+    for dir, n in ops:
         for _ in range(0, abs(n)):
-            dial += -1 if dir == 'L' else 1
+            dial += -1 if dir == "L" else 1
             dial %= 100
-            if dial == 0: password += 1
+            if dial == 0:
+                password += 1
     return password
+
 
 with open("input/day1.txt") as f:
     ops = [(x[0], int(str(x[1:]).strip())) for x in f.readlines()]
-    print(f'Part 1: {single(ops)}')
-    print(f'Part 2: {any(ops)}')
+    print(f"Part 1: {single(ops)}")
+    print(f"Part 2: {any(ops)}")
--- python/2025/day2.py
+++ python/2025/day2.py
@@ -1,20 +1,25 @@
 from collections.abc import Callable
 
+
 def partOne(n: str):
-    return n[0:len(n)//2] == n[len(n)//2:]
+    return n[0 : len(n) // 2] == n[len(n) // 2 :]
+
 
 def partTwo(n: str):
-    for div in filter(lambda div: len(n) % div == 0, range(2, len(n)+1)):
-        step = len(n)//div
-        nums = set([n[i:i+step] for i in range(0, len(n), step)])
-        if len(nums) == 1: return True
+    for div in filter(lambda div: len(n) % div == 0, range(2, len(n) + 1)):
+        step = len(n) // div
+        nums = set([n[i : i + step] for i in range(0, len(n), step)])
+        if len(nums) == 1:
+            return True
     return False
 
-def solve(inp: str, fn: Callable[[str], bool]=partOne):
+
+def solve(inp: str, fn: Callable[[str], bool] = partOne):
     ranges = [[*map(int, x.split("-"))] for x in inp.split(",")]
-    return sum(n if fn(str(n)) else 0 for (lo, hi) in ranges for n in range(lo, hi+1))
+    return sum(n if fn(str(n)) else 0 for (lo, hi) in ranges for n in range(lo, hi + 1))
+
 
 with open("input/day2.txt") as f:
     inp = f.read().strip()
-    print(f'Part 1: {solve(inp)}')
-    print(f'Part 2: {solve(inp, fn=partTwo)}')
+    print(f"Part 1: {solve(inp)}")
+    print(f"Part 2: {solve(inp, fn=partTwo)}")
